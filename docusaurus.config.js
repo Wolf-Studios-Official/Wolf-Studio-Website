895ae039206848ee5b9ce35fc6f72f08fc5b6830
Flas4hPT <93576144+TiagoVXII@users.noreply.github.com>
2023-03-03 17:51:03 +0000

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -83,36 +83,6 @@ const config = {
           hideable: true,
           autoCollapseCategories: true,
         },
-      },
-      algolia: {
-        // The application ID provided by Algolia
-        appId: 'TM0SAUQU48',
-  
-        // Public API key: it is safe to commit it
-        apiKey: '768b965525b798b487cf2ed982eb50da',
-  
-        indexName: 'wolfstudioswebnetlify',
-  
-        // Optional: see doc section below
-        contextualSearch: true,
-  
-        // Optional: Specify domains where the navigation should occur through window.location instead on history.push. Useful when our Algolia config crawls multiple documentation sites and we want to navigate with window.location.href to them.
-        externalUrlRegex: 'external\\.com|domain\\.com',
-  
-        // Optional: Replace parts of the item URLs from Algolia. Useful when using the same search index for multiple deployments using a different baseUrl. You can use regexp or string in the `from` param. For example: localhost:3000 vs myCompany.com/docs
-        replaceSearchResultPathname: {
-          from: '/docs/', // or as RegExp: /\/docs\//
-          to: '/',
-        },
-  
-        // Optional: Algolia search parameters
-        searchParameters: {},
-  
-        // Optional: path for search page that enabled by default (`false` to disable it)
-        searchPagePath: 'search',
-  
-        //... other Algolia params
-      },
       navbar: {
         title: 'Wolf Studios Website',
         logo: {
